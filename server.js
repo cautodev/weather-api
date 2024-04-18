243344f0a062f8c88b636b8ab8fd0827c3f7969c
Ryan <73195765+dorryspears@users.noreply.github.com>
2024-04-17 20:09:16 -0400

    init

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/healthcheck', (_, res) => {
+  res.send('OK')
+})
+
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`)
+  })
+}
+
+module.exports = app
