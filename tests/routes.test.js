bc1a05baf20babe98e51c0170355b68e0cf461bb
Ryan <73195765+dorryspears@users.noreply.github.com>
2024-04-19 18:17:07 -0400

    test success

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -4,6 +4,6 @@ const app = require('../server')
 describe('Get Endpoints', () => {
   it('should get a 200', async () => {
     const res = await request(app).get('/healthcheck')
-    expect(res.statusCode).toEqual(201)
+    expect(res.statusCode).toEqual(200)
   })
 })
